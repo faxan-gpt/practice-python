838ac5e711c8c27586f93a584b2ee014e21a0fc3
Mohammad Faizan <mohddfaizan001@gmail.com>
2026-08-03 23:28:15 +0530

    this is my commit 1

---
+++ q1.py
@@ -0,0 +1,10 @@
+a = input("Enter age 1: ")
+b = input("Enter age 2: ")
+c = input("Enter age 3: ")
+
+if(a >b and a> c):
+    print(" a is the oldest ")
+elif(b>c):
+    print(" b is the oldest ")
+else:
+    print(" c is the oldest ")
