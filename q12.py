abcee94bce75b987902c8c4fb32d67107f743849
Mohammad Faizan <mohddfaizan001@gmail.com>
2026-08-12 23:36:28 +0530

    Add files via upload

---
+++ q12.py
@@ -0,0 +1,8 @@
+R = float(input("Enter the radius of the cylinder: "))
+H = float(input("Enter the height of the cylinder: "))
+
+vol= 3.14*R**2*H
+print("The vol. of the cylinder is: ",vol)
+
+cost = vol*40
+print("The cost of milk here is: ", cost ,"Rs")
