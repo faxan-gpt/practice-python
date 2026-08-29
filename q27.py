70c301cd26b47cce2f52a278d817176e23d33829
Mohammad Faizan <mohddfaizan001@gmail.com>
2026-08-29 23:21:38 +0530

    Add files via upload

---
+++ q27.py
@@ -0,0 +1,12 @@
+#n = int(input("Enter the no. of odd numbers "))
+
+#sum = n*(n+1)/2
+#print(sum)
+
+
+def sum(n):
+    add = n**2
+    print (add)
+
+sum(5)
+sum(3)
---
+++ q27b.py
@@ -0,0 +1,4 @@
+n = int(input("Enter the no. of odd numbers "))
+
+sum = n**2
+print(sum)
