00cfd32fbd999063f1b1fe3398a30c1147c1ec6a
Mohammad Faizan <mohddfaizan001@gmail.com>
2026-09-02 23:41:52 +0530

    Add files via upload

--- q35.py
+++ q35.py
@@ -1,14 +1,14 @@
 def fib(n):
-    a,b = 0,1
+    a,b = 0,1 
     for _ in range(n):
-         a,b= b,a+b
-    return a
+         a,b= b,a+b # makes a as b  and b as sum of a,b # AKA function for fibonnaci series
+    return a 
 
 
 
 n = int(input("Enter your no."))
-count = 0 
-while count < n:
+count = 0 # for initalization
+while count < n: 
         print((fib(count)))
         count += 1
 
