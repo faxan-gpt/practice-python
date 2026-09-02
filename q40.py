673d5b8865474cc33bbae58f46ef0b4bd0b01466
Mohammad Faizan <mohddfaizan001@gmail.com>
2026-09-02 23:43:13 +0530

    Add files via upload

---
+++ q40.py
@@ -0,0 +1,6 @@
+n = (input("Enter your no. here: "))
+print("the no. before revrsing is " , n)
+
+rev = int(n[::-1]) # here we are not taking n as an int input becoz counting from other end does not happen in int input
+
+print("The no. after reversing is ", rev)
