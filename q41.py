9cfc4c7a6f3fb2d302d72036752e13c41b675abd
Mohammad Faizan <mohddfaizan001@gmail.com>
2026-09-03 23:28:22 +0530

    Add files via upload

---
+++ q41.py
@@ -0,0 +1,6 @@
+num = int(input("Enter your no. here: "))
+
+i = 0# intialization
+while i<num:
+    print("*" *i) # multiplies the string 
+    i += 1
---
+++ q41b.py
@@ -0,0 +1,9 @@
+def pattern(n):
+    for i in range(1,(n+1),1):
+        print("*"*i)
+        i+=1
+
+pattern(5)
+pattern(6)
+pattern(7)
+pattern(8)
